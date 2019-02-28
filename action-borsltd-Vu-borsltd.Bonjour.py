d63382b6ef4bc7ee8fa8a581030cf023ca1c92e0
Hervé Cauwelier <github@bors-ltd.fr>
2019-02-28 15:37:01 +0100

    Initial commit

---
+++ action-borsltd-Vu-borsltd.Bonjour.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import random
+
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import MqttOptions
+
+
+def subscribe_intent_callback(hermes, intent_message):
+    action_wrapper(hermes, intent_message)
+
+
+def action_wrapper(hermes, intent_message, conf):
+    """ Write the body of the function that will be executed once the intent is recognized.
+    In your scope, you have the following objects :
+    - intent_message : an object that represents the recognized intent
+    - hermes : an object with methods to communicate with the MQTT bus following the hermes protocol.
+    - conf : a dictionary that holds the skills parameters you defined.
+      To access global parameters use conf['global']['parameterName']. For end-user parameters use conf['secret']['parameterName']
+
+    Refer to the documentation for further details.
+    """
+    print('debut')
+    message = random.choice(
+        [
+            "Mon cul ? haha ! Très drôle...",
+            "Mon cul ? Je suis morte de rire...",
+            "Mon cul ? ça vous amuse ?",
+        ]
+    )
+    current_session_id = intent_message.session_id
+    hermes.publish_end_session(current_session_id, message)
+    print('fin')
+
+
+if __name__ == "__main__":
+    mqtt_opts = MqttOptions()
+    with Hermes(mqtt_options=mqtt_opts) as h:
+        h.subscribe_intent("borsltd:Vu", subscribe_intent_callback).start()
