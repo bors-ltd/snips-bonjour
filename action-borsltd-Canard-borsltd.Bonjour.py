3201b5c7065b9c294d4cef6c4b8498d23012243b
Bors, Ltd <github@bors-ltd.fr>
2019-03-02 11:01:11 +0100

    use snips-common

--- action-borsltd-Canard-borsltd.Bonjour.py
+++ action-borsltd-Canard-borsltd.Bonjour.py
@@ -4,28 +4,25 @@ import random
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import MqttOptions
 
-
-def subscribe_intent_callback(hermes, intent_message):
-    action_wrapper(hermes, intent_message)
+import snips_common
 
 
-def action_wrapper(hermes, intent_message):
-    print('debut')
-    message = random.choice(
-        [
-            "",
-            "Ah oui, je la connais celle-là.",
-            "Attendez, ça va me revenir...",
-            "On me l'a déjà racontée.",
-        ]
-    )
-    message += " Il a une patte plus courte que l'autre, surtout la gauche."
-    current_session_id = intent_message.session_id
-    hermes.publish_end_session(current_session_id, message)
-    print('fin')
+class ActionCanard(snips_common.ActionWrapper):
+    def action(self):
+        preamble = random.choice(
+            [
+                "",
+                "Ah oui, je la connais celle-là.",
+                "Attendez, ça va me revenir...",
+                "On me l'a déjà racontée.",
+            ]
+        )
+        answer = " Il a une patte plus courte que l'autre, surtout la gauche."
+        self.end_session(preamble, answer)
 
 
 if __name__ == "__main__":
     mqtt_opts = MqttOptions()
+
     with Hermes(mqtt_options=mqtt_opts) as h:
-        h.subscribe_intent("borsltd:Canard", subscribe_intent_callback).start()
+        h.subscribe_intent("borsltd:Canard", ActionCanard.callback).start()
--- action-borsltd-Vu-borsltd.Bonjour.py
+++ action-borsltd-Vu-borsltd.Bonjour.py
@@ -4,35 +4,23 @@ import random
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import MqttOptions
 
-
-def subscribe_intent_callback(hermes, intent_message):
-    action_wrapper(hermes, intent_message)
+import snips_common
 
 
-def action_wrapper(hermes, intent_message):
-    """ Write the body of the function that will be executed once the intent is recognized.
-    In your scope, you have the following objects :
-    - intent_message : an object that represents the recognized intent
-    - hermes : an object with methods to communicate with the MQTT bus following the hermes protocol.
-    - conf : a dictionary that holds the skills parameters you defined.
-      To access global parameters use conf['global']['parameterName']. For end-user parameters use conf['secret']['parameterName']
-
-    Refer to the documentation for further details.
-    """
-    print('debut')
-    message = random.choice(
-        [
-            "Mon cul ? haha ! Très drôle...",
-            "Mon cul ? Je suis morte de rire...",
-            "Mon cul ? ça vous amuse ?",
-        ]
-    )
-    current_session_id = intent_message.session_id
-    hermes.publish_end_session(current_session_id, message)
-    print('fin')
+class ActionVu(snips_common.ActionWrapper):
+    def action(self):
+        message = random.choice(
+            [
+                "Mon cul ? haha ! Très drôle...",
+                "Mon cul ? Je suis morte de rire...",
+                "Mon cul ? ça vous amuse ?",
+            ]
+        )
+        self.end_session(message)
 
 
 if __name__ == "__main__":
     mqtt_opts = MqttOptions()
+
     with Hermes(mqtt_options=mqtt_opts) as h:
-        h.subscribe_intent("borsltd:Vu", subscribe_intent_callback).start()
+        h.subscribe_intent("borsltd:Vu", ActionVu.callback).start()
