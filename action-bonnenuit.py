6d38652de409fe9e6f16c70f633ebafac6c62b23
Bors, Ltd <github@bors-ltd.fr>
2019-04-06 10:01:07 +0200

    rather use a thread

--- action-bonnenuit.py
+++ action-bonnenuit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import os
-import time
+import threading
 
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import MqttOptions
@@ -8,13 +8,17 @@ from hermes_python.ontology import MqttOptions
 import snips_common
 
 
+def poweroff():
+    # "sudo visudo" and add "_snips-skills ALL = NOPASSWD: /sbin/poweroff"
+    os.system("sudo poweroff")
+
+
 class ActionVu(snips_common.ActionWrapper):
     def action(self):
         self.end_session("Bonne nuit")
-        # Wait a little for the message to be said
-        time.sleep(5)
-        # "sudo visudo" and add "_snips-skills ALL = NOPASSWD: /sbin/poweroff"
-        os.system("sudo poweroff")
+        # Wait enough for the message to be said
+        timer = threading.Timer(10, poweroff)
+        timer.start()
 
 
 if __name__ == "__main__":
